6c339f7e19273ee3001ed68a2f540e30d0d82d0d
UBF21 <flashgames104@gmail.com>
2022-06-04 01:57:09 -0500

    Retoque  de pagina de presentacion y navegación

---
+++ Controllers/PresentacionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cineplus_DSW_Proyecto.Controllers
+{
+    public class PresentacionController : Controller
+    {
+        public IActionResult presentacion()
+        {
+            return View();
+        }
+    }
+}
